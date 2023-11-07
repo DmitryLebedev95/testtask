37209489028fa21cd59eca4e499a9eba160d8546
Дмитрий Лебедев <d.lebedev@maxbit.local>
2023-11-07 10:43:25 +0300

    Небольшие правки

--- page_objects/tournaments.js
+++ page_objects/tournaments.js
@@ -1,12 +1,17 @@
 class Tournaments {
     constructor() {
         this.elements = {
-            SLOTOPOLIS: this.slotopolisElement
+            SLOTOPOLIS: this.slotopolisElement,
+            ROXSTARS: this.roxStarElement
         }
     }
 
     get slotopolisElement() {
         return '//a[@href=\'/tournaments/slotopolis\']';
+    }
+
+    get roxStarElement() {
+        return '//a[@href=\'/tournaments/roxstars\']';
     }
 }
 
